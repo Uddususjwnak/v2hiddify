2a5163d6d59e577b1d76837faa20944039faf72d
Uddususjwnak <luodiyemian@gmail.com>
2024-11-09 23:56:45 +0800

    Update package_windows.ps1

--- scripts/package_windows.ps1
+++ scripts/package_windows.ps1
@@ -3,8 +3,8 @@ New-Item -ItemType Directory -Force -Name "out"
 
 # windows setup
 # Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "dist\tmp\hiddify-next-setup.exe" -ErrorAction SilentlyContinue
-# Compress-Archive -Force -Path "dist\tmp\hiddify-next-setup.exe",".github\help\mac-windows\*.url" -DestinationPath "out\hiddify-windows-x64-setup.zip"
-Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "out\Hiddify-Windows-Setup-x64.exe" -ErrorAction SilentlyContinue
+# Compress-Archive -Force -Path "dist\tmp\hiddify-next-setup.exe",".github\help\mac-windows\*.url" -DestinationPath "out\ZiYouTo-windows-x64-setup.zip"
+Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "out\ZiYouTo-Windows-Setup-x64.exe" -ErrorAction SilentlyContinue
 Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows.msix" | Copy-Item -Destination "out\Hiddify-Windows-Setup-x64.msix" -ErrorAction SilentlyContinue
 
 
