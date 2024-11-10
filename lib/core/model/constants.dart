ddace1978b453acf94928ce2657a27e033f4edcd
Uddususjwnak <luodiyemian@gmail.com>
2024-11-10 16:13:01 +0800

    Update constants.dart

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,13 +1,13 @@
 abstract class Constants {
   static const appName = "ZIYOUTO";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
+  static const githubUrl = "https://ziyouto.九品堂.com/";
   static const githubReleasesApiUrl =
       "https://api.github.com/repos/hiddify/hiddify-next/releases";
   static const githubLatestReleaseUrl =
       "https://github.com/hiddify/hiddify-next/releases/latest";
   static const appCastUrl =
       "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://ziyouto.九品堂.com/";
+  static const telegramChannelUrl = "https://w.ziyouto.org/";
   static const privacyPolicyUrl = "https://privacy.ziyouto.us/";
   static const termsAndConditionsUrl = "https://hiddify.com/terms/";
   static const cfWarpPrivacyPolicy =
